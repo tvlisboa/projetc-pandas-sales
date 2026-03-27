be85632698ca9464e75fe2633558bc99c344ba46
tvlisboa <tvlisboa@hotmail.com>
2026-03-26 22:16:32 -0300

    database tratament with columns

--- rsvoicecloud.py
+++ rsvoicecloud.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
 #passo 1 - importar base de dados
+tabela_instalacao = pd.read_excel('teste_erictel.xlsx')
+
 #passo 2 - visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_instalacao)
 
 #quantidade total de aparelhos instalados
 
@@ -11,4 +17,6 @@
 
 #enviar relatorio por email
 
-print("nothing is gonna happing")
+
+
+print("26 de marco de 2026")
