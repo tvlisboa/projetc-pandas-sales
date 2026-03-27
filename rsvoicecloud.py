4a96e517e187ecb82a8d1cb5dc20433213bbf8cb
tvlisboa <tvlisboa@hotmail.com>
2026-03-26 21:56:32 -0300

    replace db_rsvoice and new python archive

---
+++ rsvoicecloud.py
@@ -0,0 +1,14 @@
+#passo 1 - importar base de dados
+#passo 2 - visualizar a base de dados
+
+#quantidade total de aparelhos instalados
+
+#quantidade total por tecnico
+
+#ticket medio de instalacao semanal por tecnico
+
+#quantidade descriminada por telefones instalados
+
+#enviar relatorio por email
+
+print("nothing is gonna happing")
